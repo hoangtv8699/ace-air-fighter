24b6d6e7402595962487d749e90941b9ae422102
Tran Van Hoang <hoang.tv173129@sis.hust.edu.vn>
2020-06-20 10:08:13 +0700

    update harder at each level

--- Assets/Game/Standard Assets/Utility/AutoMoveAndRotate.cs
+++ Assets/Game/Standard Assets/Utility/AutoMoveAndRotate.cs
@@ -8,12 +8,34 @@ namespace UnityStandardAssets.Utility
         public Vector3andSpace moveUnitsPerSecond;
         public Vector3andSpace rotateDegreesPerSecond;
         public bool ignoreTimescale;
+        public float speed = 1f;
         private float m_LastRealTime;
+        private Client client;
+        
         
 
 
         private void Start()
         {
+            if (gameObject.tag.Equals("Enemy"))
+            {
+                client = GameObject.FindGameObjectWithTag("client").GetComponent<Client>();
+                int level = client.getInt("LevelSelect");
+
+                switch (level)
+                {
+                    case 1:
+                        speed = 1.3f;
+                        break;
+                    case 2:
+                        speed = 1.6f;
+                        break;
+                    case 3:
+                        speed = 2f;
+                        break;
+              
+                }
+            }
             
             m_LastRealTime = Time.realtimeSinceStartup;
         }
@@ -28,7 +50,7 @@ namespace UnityStandardAssets.Utility
                 deltaTime = (Time.realtimeSinceStartup - m_LastRealTime);
                 m_LastRealTime = Time.realtimeSinceStartup;
             }
-            transform.Translate(moveUnitsPerSecond.value*deltaTime, moveUnitsPerSecond.space);
+            transform.Translate(moveUnitsPerSecond.value*deltaTime*speed, moveUnitsPerSecond.space);
             transform.Rotate(rotateDegreesPerSecond.value*deltaTime, moveUnitsPerSecond.space);
         }
 
--- Assets/Game/_Scripts/Controller/EnemyController.cs
+++ Assets/Game/_Scripts/Controller/EnemyController.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
+    public float RepeatTime = 2f;
+    private Client client;
 
 	public GunController  thisGunController;
 	void Start ()
 	{
-		thisGunController = GetComponent<GunController> ();
+        client = GameObject.FindGameObjectWithTag("client").GetComponent<Client>();
+        int level = client.getInt("LevelSelect");
 
-        InvokeRepeating("FireBullets", 1.0f, 2.0f);
+        switch (level)
+        {
+            case 1:
+                RepeatTime = 2f;
+                break;
+            case 2:
+                RepeatTime = 1f;
+                break;
+            case 3:
+                RepeatTime = 0.5f;
+                break;
+        }
+
+        thisGunController = GetComponent<GunController> ();
+
+        InvokeRepeating("FireBullets", 1.0f, RepeatTime);
 	}
 
 	void FireBullets ()
